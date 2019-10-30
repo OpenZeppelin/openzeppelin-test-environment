3bb8a15501ba38b44413a3f899170bcb5275391f
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-10-30 19:43:47 -0300

    Add @openzeppelin/contract-loader, remove default account from exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import isHelpersConfigured from './helpers';
 import { accounts, web3, provider } from './web3';
-import load from './load';
 
-export { accounts, web3, provider, load, isHelpersConfigured };
+// We use the first account as the default sender (when no sender is specified),
+// which provides versatility for tests where this sender is not important
+// (e.g. when calling view functions).
+// We also don't expose this account so that it is not possible to explicitly
+// use it, creating a scenario where the default account and an explicit account
+// are the same one, which can create hard to debug failing tests.
+
+const defaultSender = accounts[0];
+const exposedAccounts = accounts.slice(1);
+
+const loaderConfig = {
+  web3Contract: web3.eth.Contract,
+  defaultSender,
+};
+
+const load = require('@openzeppelin/contract-loader')(loaderConfig);
+
+module.exports = {
+  accounts: exposedAccounts,
+  web3,
+  provider,
+  load,
+  isHelpersConfigured,
+};
--- src/load.ts
+++
@@ -1,8 +0,0 @@
-import { web3 } from './web3';
-import fs from 'fs';
-import { Contract } from 'web3-eth-contract';
-
-export default function(contract: string): Contract {
-  const { abi, bytecode } = JSON.parse(fs.readFileSync(`./build/contracts/${contract}.json`, 'utf8'));
-  return new web3.eth.Contract(abi, undefined, { data: bytecode });
-}
