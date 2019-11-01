b4d55f0fb46dfbea92481cc940fe32afe7adcfb8
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-01 00:09:16 -0300

    Fix regular mocha tests

--- test/mocha/test/accounts.test.js
+++
@@ -1,8 +0,0 @@
-const { accounts } = require('@openzeppelin/test-env');
-const { expect } = require('chai');
-
-describe('accounts', function() {
-  it('there are ten accounts', async function() {
-    expect(accounts.length).to.equal(10);
-  });
-});
