f8badf3673acef1b4d519a4112cd92dc65988d88
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-13 19:16:53 -0300

    Add note about port not being configured

--- src/ganache-server.ts
+++ src/ganache-server.ts
@@ -10,10 +10,15 @@ function send(msg: GanacheServer.Message): void {
 }
 
 process.once('message', options => {
-  const { port, accountsConfig, gasLimit } = options;
+  const { accountsConfig, gasLimit } = options;
   const server = ganache.server({ accounts: accountsConfig, gasLimit });
 
-  server.listen(port, function(err: unknown) {
+  // An undefined port number makes ganache-core choose a random free port,
+  // which plays nicely with environments such as jest and ava, where multiple
+  // processes of test-env may be run in parallel.
+  // It also means however that the port (and therefore host URL) is not
+  // available until the server finishes initialization.
+  server.listen(undefined, function(err: unknown) {
     if (err) {
       send({ type: 'error' });
     } else {
