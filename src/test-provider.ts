97a1c486adb049183fc5aa40b610f9cf3b51ef53
Igor Yalovoy <igor@yalovoi.net>
2019-11-13 13:27:15 +0300

    Use PQueue for TestProvider

--- src/test-provider.ts
+++ src/test-provider.ts
@@ -1,3 +1,5 @@
+import PQueue from 'p-queue';
+
 import Web3 from 'web3';
 import { Provider, JsonRPCRequest, Callback, JsonRPCResponse } from 'web3/providers';
 
@@ -5,40 +7,30 @@ import setupGanache from './setup-ganache';
 import config from './config';
 
 export default class TestProvider implements Provider {
+  private queue: PQueue;
   private wrappedProvider?: Provider;
   private sendAsync: (payload: JsonRPCRequest, callback: Callback<JsonRPCResponse>) => void;
 
   constructor() {
     this.sendAsync = this.send.bind(this);
+    this.queue = new PQueue({ concurrency: 1 });
+
+    this.queue.add(async () => {
+      // Setup node
+      const url = await setupGanache();
+      // Create base provider (connection to node)
+      const baseProvider = new Web3(url).eth.currentProvider;
+
+      // Create a custom provider (e.g. GSN provider) and wrap it
+      this.wrappedProvider = await config.setupProvider(baseProvider as Provider);
+    });
   }
 
   public send(payload: JsonRPCRequest, callback: Callback<JsonRPCResponse>): void {
-    if (this.wrappedProvider) {
-      this.wrappedProvider.send(payload, callback);
-    } else {
-      // Setup node
-      setupGanache()
-        .then(
-          url => {
-            // Create base provider (connection to node)
-            const baseProvider = new Web3(url).eth.currentProvider;
-
-            // Create a custom provider (e.g. GSN provider) and wrap it
-            return config.setupProvider(baseProvider as Provider);
-          },
-          error => {
-            throw new Error(`Failed to start a local node: ${error}`);
-          },
-        )
-        .then(
-          provider => {
-            this.wrappedProvider = provider;
-            this.wrappedProvider.send(payload, callback);
-          },
-          error => {
-            throw new Error(`Failed to wrap a provider: ${error}`);
-          },
-        );
-    }
+    this.queue.onIdle().then(() => {
+      // wrapped provider is always not null due to PQueue running provider initialization
+      // before any send calls yet TypeScript can't possibly know that
+      this.wrappedProvider && this.wrappedProvider.send(payload, callback);
+    });
   }
 }
