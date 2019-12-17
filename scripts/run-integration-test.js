ef6cd3cbf9979db03b3a3354f78db7cacb19a72e
Francisco Giordano <frangio.1@gmail.com>
2019-12-17 17:02:37 -0300

    Review integration tests setup (#30)
    * remove unnecessary npx
    
    * improve integration test setup and circle config
    
    * fix yaml syntax error
    
    * change parameter name
    
    * begin js impl
    
    * add prepacking of tarball
    
    * update oz-docs-utils to fix netlify errors
    
    * fix circle build error
    
    * remove minimist depdency from integration test script
    
    * add tarballs to gitignore
    
    * fix bugs
    
    * disable linter
    
    * make run command implicit
    
    * remove console.error
    
    * improve logs
    
    * remove chalk since dependencies are not installed for integration tests

---
+++ scripts/run-integration-test.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+
+const path = require('path');
+const { promises: fs } = require('fs');
+const { promisify } = require('util');
+const { once } = require('events');
+const proc = require('child_process');
+const execFile = promisify(proc.execFile);
+
+const args = process.argv.slice(2);
+
+if (args[0] === 'run-tarball') {
+  run(args.slice(2), args[1]);
+} else if (args[0] === 'pack') {
+  pack(args[1]);
+} else {
+  run(args);
+}
+
+async function run(tests, package) {
+  if (tests.length === 0) {
+    tests = await fs.readdir('test-integration');
+  } else {
+    for (const test of tests) {
+      if (!await exists(`test-integration/${test}`)) {
+        throw new Error(`Integration test '${test}' does not exist`);
+      }
+    }
+  }
+
+  if (package === undefined) {
+    package = await pack(package);
+  } else if (!await exists(package)) {
+    console.error(`File not found: '${package}'\n`);
+    process.exit(1);
+  }
+
+  for (const test of tests) {
+    console.error(`[test:integration running ${test}]`);
+    const cwd = `test-integration/${test}`;
+    if (!await exists(`${cwd}/node_modules`)) {
+      await spawn('npm', ['ci'], { cwd, stdio: 'inherit' });
+    }
+
+    await spawn('npm', ['install', '--no-save', path.resolve(package)], { cwd, stdio: 'inherit' });
+    await spawn('npm', ['test'], { cwd, stdio: 'inherit' });
+  }
+}
+
+async function pack(dest) {
+  console.error(`[test:integration packing tarball]`);
+  const { stdout } = await execFile('npm', ['pack']);
+  const package = stdout.match(/\n(.+)\n$/)[1];
+  if (dest === undefined) {
+    return package;
+  } else {
+    await fs.mkdir(path.dirname(dest), { recursive: true });
+    await fs.rename(package, dest);
+    return dest;
+  }
+}
+
+async function exists(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
+async function spawn(...args) {
+  const child = proc.spawn(...args);
+  const [code] = await once(child, 'exit');
+  if (code !== 0) {
+    throw new Error(`Process exited with an error`);
+  }
+}
--- test-integration/run-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for dir in */
-do
-  ./run-test.sh ${dir%*/}
-done
