fe01e089d62f8bb86fd30b121d62d676a612122f
Miguel Lo-A-Foe <epiccoolguy@users.noreply.github.com>
2019-12-22 20:13:59 +0100

    Publish types for TypeScript users (#33)
    * Add generating declaration files
    
    * Use esmodule export to allow proper declarations to be emitted
    
    * Define package types
    
    * Ignore test file declarations
    
    * bump package version
    
    * Revert "bump package version"
    
    This reverts commit 1c873773b2476dd3b99fb6af9caf4aa638aa1cd3.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,4 @@ import { web3, provider } from './setup-provider';
 import { accounts, defaultSender } from './accounts';
 import contract from './setup-loader';
 
-module.exports = {
-  accounts,
-  defaultSender,
-  web3,
-  provider,
-  contract,
-  isHelpersConfigured,
-};
+export { accounts, defaultSender, web3, provider, contract, isHelpersConfigured };
