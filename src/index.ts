11e1be6083ab3e219227459905166eccb4ad6585
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2020-03-09 02:02:52 +0100

    Revert "export accountsConfig to allow use of private keys for web3.eth.sendSignedTransaction (eth_sendRawTransaction)"
    This reverts commit 8d4149f6ba51a7273f18ddf57395c1f7c2412ea3.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import isHelpersConfigured from './helpers';
 import { web3, provider } from './setup-provider';
-import { accounts, accountsConfig, defaultSender } from './accounts';
+import { accounts, defaultSender } from './accounts';
 import contract from './setup-loader';
 
-export { accounts, accountsConfig, defaultSender, web3, provider, contract, isHelpersConfigured };
+export { accounts, defaultSender, web3, provider, contract, isHelpersConfigured };
