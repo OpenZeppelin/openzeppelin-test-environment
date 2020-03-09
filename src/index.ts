751be8da028599a2dfe583db19804663de5e5da7
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2020-03-09 02:08:12 +0100

    less intrusive approach, exposing generated private keys in the same way as the accounts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import isHelpersConfigured from './helpers';
 import { web3, provider } from './setup-provider';
-import { accounts, defaultSender } from './accounts';
+import { accounts, privateKeys, defaultSender } from './accounts';
 import contract from './setup-loader';
 
-export { accounts, defaultSender, web3, provider, contract, isHelpersConfigured };
+export { accounts, privateKeys, defaultSender, web3, provider, contract, isHelpersConfigured };
