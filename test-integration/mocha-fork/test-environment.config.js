6e2ea2d350f1412a24424e0f6109ae6908d98771
Igor Yalovoy <to@ylv.io>
2020-04-02 18:48:51 -0300

    Add tests for unlocked_accounts option

--- test-integration/mocha-fork/test-environment.config.js
+++ test-integration/mocha-fork/test-environment.config.js
@@ -8,5 +8,6 @@ module.exports = {
 
   node: {
     fork: 'https://mainnet.infura.io/v3/95202223388e49f48b423ea50a70e336',
+    unlocked_accounts: ['0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B'],
   },
 };
--- test-integration/mocha-fork/test/config.test.js
+++ test-integration/mocha-fork/test/config.test.js
@@ -1,7 +1,10 @@
 const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+const BN = require('bn.js');
 const { expect } = require('chai');
 
 const DAI = contract.fromArtifact('Dai');
+
+const { first } = accounts;
 
 describe('forked blockchain', function() {
   it('gets correct name for DAI stablecoin', async function() {
@@ -12,5 +15,13 @@ describe('forked blockchain', function() {
   it('gets a contracts bytecode', async function() {
     const code = await web3.eth.getCode('0x6B175474E89094C44Da98b954EedeAC495271d0F');
     expect(code).to.match(/0x608060405234801561001057600080fd5b50600436106101425760003560e01c80637ecebe001/);
+  });
+  it('can spend Vitalik ether', async function() {
+    const hash = await web3.eth.sendTransaction({
+      from: '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B',
+      to: first,
+      value: web3.utils.toWei('1000', 'ether'),
+    });
+    expect(hash.transactionHash).to.not.be.undefined;
   });
 });
