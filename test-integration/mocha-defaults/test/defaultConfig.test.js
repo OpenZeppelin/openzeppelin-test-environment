c181c50c7f6d642d270be6ed536b21ab36e29d6e
Igor Yalovoy <to@ylv.io>
2020-04-01 23:24:09 -0300

    Add unlimited contract size test to defaultConfig.test.js

--- test-integration/mocha-defaults/test/defaultConfig.test.js
+++ test-integration/mocha-defaults/test/defaultConfig.test.js
@@ -1,37 +1,46 @@
-const { accounts, provider, web3, defaultSender } = require('@openzeppelin/test-environment');
+const { accounts, provider, contract, web3, defaultSender } = require('@openzeppelin/test-environment');
 const { expect } = require('chai');
 
 const uniq = require('lodash.uniq');
 
+const Fatty = contract.fromArtifact('Fatty');
+
 describe('default config', function() {
-  describe('accounts', function () {
-    it('there are ten unlocked accounts', function () {
+  describe('accounts', function() {
+    it('there are ten unlocked accounts', function() {
       expect(accounts.length).to.equal(10);
     });
 
-    it('all accounts are different', function () {
+    it('all accounts are different', function() {
       expect(uniq(accounts).length).to.equal(10);
     });
 
-    it('accounts initial balance is 100 ether', async function () {
+    it('accounts initial balance is 100 ether', async function() {
       // We check the balance of the last account, in the hope that it hasn't been used yet and therefore has the full
       // unspent initial balance
       expect(await web3.eth.getBalance(accounts[accounts.length - 1])).to.equal((100 * 1e18).toString());
     });
 
-    it('the default sender is not on the accounts array', async function () {
+    it('the default sender is not on the accounts array', async function() {
       expect(accounts).to.not.include(defaultSender);
     });
   });
 
-  describe('blockchain', function () {
-    it('gasLimit is 8 000 000', async function () {
+  describe('blockchain', function() {
+    it('gasLimit is 8 000 000', async function() {
       await web3.eth.sendTransaction({ from: accounts[0], to: accounts[1], gas: 8000000 });
 
       try {
         await web3.eth.sendTransaction({ from: accounts[0], to: accounts[1], gas: 8000001 });
         expect.fail('Transaction over gas limit was executed');
-      } catch { };
+      } catch {}
+    });
+
+    it('fails to deploy a contract over the limit', async function() {
+      try {
+        const fatty = await Fatty.new();
+        expect.fail('Attempted to deploy a contract over the limit');
+      } catch {}
     });
   });
 });
