dd4ff8e7e51d33e2189221c661907fe937d77115
Igor Yalovoy <to@ylv.io>
2020-06-25 23:56:36 +0300

    Export config

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { web3, provider } from './setup-provider';
 import { accounts, privateKeys, defaultSender } from './accounts';
 import contract from './setup-loader';
 import { runCoverage } from './coverage';
+import config from './config';
 
-export { accounts, privateKeys, defaultSender, web3, provider, contract, isHelpersConfigured, runCoverage };
+export { accounts, privateKeys, defaultSender, web3, provider, contract, isHelpersConfigured, runCoverage, config };
